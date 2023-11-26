c3d50090eb29856ea0335cb257a0d3712c14215a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:48:51 +0500

    sfklajndnlkasd jakjf kaf jldladsakadk

---
+++ Appartment-Application/Repositories/ReservationRepositories/ReservationRepository.cs
@@ -0,0 +1,33 @@
+﻿using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+namespace Appartment_Application.Repositories.ReservationRepositories
+{
+    public class ReservationRepository : IReservationRepository
+    {
+        public ValueTask<int> CreateAsync(ReservationDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> DeleteAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<List<Reservation>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<Reservation> GetByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValueTask<int> UpdateAsync(int Id, ReservationDto model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
