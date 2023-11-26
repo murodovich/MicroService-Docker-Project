869d9cf3f9eeb4fcba64dedf6d5638f8937f38c2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:49:03 +0500

    j kja k akdkfk

---
+++ Appartment-Application/Repositories/UserApartmentRepositories/UserApartmentRepository.cs
@@ -0,0 +1,39 @@
+﻿using Appartment_Domain.Data;
+using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+namespace Appartment_Application.Repositories.UserApartmentRepositories;
+public class UserApartmentRepository : IUserApartmentRepository
+{
+    private readonly ApartmentDBContext _dbContext;
+
+    public UserApartmentRepository(ApartmentDBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public ValueTask<int> CreateAsync(UserApartmentDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<int> DeleteAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<List<UserApartment>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<UserApartment> GetByIdAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<int> UpdateAsync(int Id, UserApartmentDto model)
+    {
+        throw new NotImplementedException();
+    }
+}
