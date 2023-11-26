627ebd9662f78538d0ce48167a7fb09701da3257
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:28:02 +0500

    sdvfa

---
+++ Appartment-Application/Migrations/20231125133256_Apartment1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appartment_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class Apartment1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserImage",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserImage",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
