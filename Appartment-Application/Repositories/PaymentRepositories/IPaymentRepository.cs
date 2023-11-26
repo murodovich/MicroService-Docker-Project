87a6448677fc6a562d6d730a6cacd290f0bcdbdc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:48:31 +0500

    kjlvjalnfcas da

---
+++ Appartment-Application/Repositories/PaymentRepositories/IPaymentRepository.cs
@@ -0,0 +1,7 @@
+﻿using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+namespace Appartment_Application.Repositories.PaymentRepositories;
+public interface IPaymentRepository : IBaseRepository<Payment, PaymentDto>
+{
+}
