bf4903b3b62fd4cc45eab9c617357c830b59e193
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:48:26 +0500

    jsffbjkandl

---
+++ Appartment-Application/Repositories/ApartmentRepositories/IApartmentRepository.cs
@@ -0,0 +1,7 @@
+﻿using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+namespace Appartment_Application.Repositories.ApartmentRepositories;
+public interface IApartmentRepository : IBaseRepository<Apartment,ApartmentDto>
+{
+}
