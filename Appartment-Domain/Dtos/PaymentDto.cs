93198946387db4a23b8eb91f6fecb4968040efaa
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:49:18 +0500

    jkbdskja lvkakj

---
+++ Appartment-Domain/Dtos/PaymentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Appartment_Domain.Dtos;
+public class PaymentDto
+{
+    public int ReservationId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public string PaymentStatus { get; set; }
+}
