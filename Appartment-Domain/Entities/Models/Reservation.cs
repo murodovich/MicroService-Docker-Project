d598fedb21a77fba5db0302884bf85333bb79731
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:28:55 +0500

    svs

---
+++ Appartment-Domain/Entities/Models/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Appartment_Domain.Entities.Models
+{
+    public class Reservation
+    {
+        [ForeignKey(nameof(ReservationId))]
+        public int ReservationId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+
+        public List<UserApartment> UserApartments { get; set; }
+    }
+}
