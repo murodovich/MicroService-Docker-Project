0a98e11225c73586a295fcd49b22b08fe0b33700
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:47:59 +0500

    mjkfdnkjsvddjl

---
+++ Appartment-Api/Controllers/ApartmentController.cs
@@ -0,0 +1,57 @@
+﻿
+using Appartment_Application.Repositories.ApartmentRepositories;
+using Appartment_Domain.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appartment_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ApartmentController : ControllerBase
+    {
+        private readonly IApartmentRepository _repository;
+
+        public ApartmentController(IApartmentRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult ApartmentGetAll()
+        {
+            var result = _repository.GetAllAsync();
+            return Ok(result.Result);
+
+        }
+
+        [HttpPost]
+        public IActionResult ApartmentCreat(ApartmentDto dto)
+        {
+            var result = _repository.CreateAsync(dto);
+            return Ok(result.Result);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ApartmentGetById(int id)
+        {
+            var result = _repository.GetByIdAsync(id);
+            return Ok(result.Result);
+        }
+
+        [HttpPut]
+        public IActionResult ApartmentUpdate(int id, ApartmentDto dto)
+        {
+            var result = _repository.UpdateAsync(id, dto);
+            return Ok(result.Result);
+        }
+        [HttpDelete]
+        public IActionResult ApartmentDeleted(int id)
+        {
+            var result = _repository.DeleteAsync(id);
+            return Ok(result.Result);
+        }
+    }
+}
