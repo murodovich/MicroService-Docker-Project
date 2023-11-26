c104632feb71355f730e5a5bf828560e7a404ff1
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:49:38 +0500

    j cksa kaskjsd

---
+++ Appartment-Infrastructure/Services/UserServices/IUserService.cs
@@ -0,0 +1,7 @@
+﻿using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+namespace Appartment_Infrastructure.Services.UserServices;
+public interface IUserService : IBaseService<User,UserDto>
+{
+}
