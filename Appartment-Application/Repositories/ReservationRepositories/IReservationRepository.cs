bd2ddeaeeafc12815c49f75e39d2f1bc04e543b5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:48:43 +0500

    jkzbdjka fafkj

---
+++ Appartment-Application/Repositories/ReservationRepositories/IReservationRepository.cs
@@ -0,0 +1,7 @@
+﻿using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+namespace Appartment_Application.Repositories.ReservationRepositories;
+public interface IReservationRepository : IBaseRepository<Reservation,ReservationDto>
+{
+}
