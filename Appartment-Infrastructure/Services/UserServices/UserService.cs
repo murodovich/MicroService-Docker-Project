aba0e7e4fde8e1b563d1e798ee35f119eb7c85ee
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 23:49:43 +0500

    sjbjkca kjcakc v

---
+++ Appartment-Infrastructure/Services/UserServices/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Appartment_Domain.Dtos;
+using Appartment_Domain.Entities.Models;
+
+
+namespace Appartment_Infrastructure.Services.UserServices;
+public class UserService : IUserService
+{
+    
+    public ValueTask<int> CreateAsync(UserDto model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<int> DeleteAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<List<User>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<User> GetByIdAsync(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask<int> UpdateAsync(int Id, UserDto model)
+    {
+        throw new NotImplementedException();
+    }
+}
