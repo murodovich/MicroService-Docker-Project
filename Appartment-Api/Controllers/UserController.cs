3b1f6e6622cc78c4957616d9bef08d0883ba4434
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:27:04 +0500

    zcnz

---
+++ Appartment-Api/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Appartment_Application.Repositories.UserRepositories;
+using Appartment_Domain.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appartment_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userrepository;
+
+        public UserController(IUserRepository userrepository)
+        {
+            _userrepository = userrepository;
+        }
+
+        [HttpGet]
+        public IActionResult UserGetAll()
+        {
+            var result = _userrepository.GetAllAsync();
+            return Ok(result.Result);
+        }
+        [HttpPost]
+        public IActionResult UserCreate(UserDto userDto)
+        {
+            var result = _userrepository.CreateAsync(userDto);
+            return Ok(result.Result);
+        }
+    }
+}
