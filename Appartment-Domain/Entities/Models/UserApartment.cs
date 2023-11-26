57394a96e42ac3a411c6a93117b4a4ce68cc29dc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:29:04 +0500

    fads

---
+++ Appartment-Domain/Entities/Models/UserApartment.cs
@@ -0,0 +1,12 @@
+﻿namespace Appartment_Domain.Entities.Models
+{
+    public class UserApartment
+    {
+        public int UserApartmentId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int ApartmentId { get; set; }
+        public Apartment Apartment { get; set; }
+    }
+}
