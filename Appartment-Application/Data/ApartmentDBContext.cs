52ed65153efdc1b18dd3a0e1aa9a29454fc40e99
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:27:48 +0500

    jyrhtg

---
+++ Appartment-Application/Data/ApartmentDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Appartment_Domain.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Appartment_Domain.Data
+{
+    public class ApartmentDBContext : DbContext
+    {
+        public ApartmentDBContext(DbContextOptions<ApartmentDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Apartment> Apartments { get; set; }
+        public DbSet<Payment> payments { get; set; }
+        public DbSet<Reservation> reservations { get; set; }
+
+
+        
+
+    }
+}
