edfa778b01c039e3a76495900696662285a08fba
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:28:59 +0500

    fbvsda

---
+++ Appartment-Domain/Entities/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Appartment_Domain.Entities.Models
+{
+    public class User
+    {
+        [ForeignKey(nameof(UserId))]
+        public  int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        public List<UserApartment> UserApartments { get; set; }
+    }
+}
