64cecfe0742144ba7334a3a3a773f85e38ce953c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-26 12:28:50 +0500

    dbsvfa

---
+++ Appartment-Domain/Entities/Models/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Appartment_Domain.Entities.Models
+{
+    public class Payment
+    {
+        [ForeignKey(nameof(PaymentId))]
+        public int PaymentId { get; set; }
+        public int ReservationId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string PaymentStatus { get; set; }
+    }
+}
+
