20df890c392be3d14e564b600ba13b47d43bb556
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:46:07 +0500

    dada

---
+++ Car-Application/Migrations/20231127071555_CarDB.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Car_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class CarDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "manufacturer",
+                columns: table => new
+                {
+                    ManufacturerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_manufacturer", x => x.ManufacturerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "owner",
+                columns: table => new
+                {
+                    OwnerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_owner", x => x.OwnerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "cars",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ManufacturerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_cars", x => x.CarId);
+                    table.ForeignKey(
+                        name: "FK_cars_manufacturer_ManufacturerId",
+                        column: x => x.ManufacturerId,
+                        principalTable: "manufacturer",
+                        principalColumn: "ManufacturerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ownerCars",
+                columns: table => new
+                {
+                    OwnerId = table.Column<int>(type: "int", nullable: false),
+                    CarId = table.Column<int>(type: "int", nullable: false),
+                    OwnerCarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ownerCars", x => new { x.OwnerId, x.CarId });
+                    table.ForeignKey(
+                        name: "FK_ownerCars_cars_CarId",
+                        column: x => x.CarId,
+                        principalTable: "cars",
+                        principalColumn: "CarId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ownerCars_owner_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "owner",
+                        principalColumn: "OwnerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_cars_ManufacturerId",
+                table: "cars",
+                column: "ManufacturerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ownerCars_CarId",
+                table: "ownerCars",
+                column: "CarId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ownerCars");
+
+            migrationBuilder.DropTable(
+                name: "cars");
+
+            migrationBuilder.DropTable(
+                name: "owner");
+
+            migrationBuilder.DropTable(
+                name: "manufacturer");
+        }
+    }
+}
