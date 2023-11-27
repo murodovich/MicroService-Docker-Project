044c7e24f8ff7829ae4def97bd230b7f45996f73
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:46:44 +0500

    dada

---
+++ Car-Application/Repositories/OwnerCarRepositories/OwnerCarRepository.cs
@@ -0,0 +1,58 @@
+﻿using Car_Application.Data;
+using Car_Domain.Dtos;
+using Car_Domain.Entiries.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Application.Repositories.OwnerCarRepositories;
+public class OwnerCarRepository : IOwnerCarRepository
+{
+    private readonly CarDBContext _dbContext;
+
+    public OwnerCarRepository(CarDBContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async ValueTask<int> CreateAsync(OwnerCarDto model)
+    {
+        OwnerCar ownerCar = new OwnerCar();
+        ownerCar.CarId = model.CarId;
+        ownerCar.OwnerId = model.OwnerId;
+
+        await _dbContext.ownerCars.AddAsync(ownerCar);
+        var result = await _dbContext.SaveChangesAsync();
+        return result;
+    }
+
+    public async ValueTask<int> DeleteAsync(int Id)
+    {
+        var result = await _dbContext.ownerCars.FirstOrDefaultAsync(x => x.OwnerCarId == Id);
+
+        _dbContext.ownerCars.Remove(result);
+        var res = await _dbContext.SaveChangesAsync();
+        return res;
+    }
+
+    public async ValueTask<List<OwnerCar>> GetAllAsync()
+    {
+        var result = await _dbContext.ownerCars.ToListAsync();
+        return result;
+    }
+
+    public async ValueTask<OwnerCar> GetByIdAsync(int Id)
+    {
+        var result = await _dbContext.ownerCars.FirstOrDefaultAsync(x => x.OwnerCarId == Id);
+        return result;
+    }
+
+    public async ValueTask<int> UpdateAsync(int Id, OwnerCarDto model)
+    {
+        var result = await _dbContext.ownerCars.FirstOrDefaultAsync(x => x.OwnerCarId == Id);
+        result.CarId = model.CarId;
+        result.OwnerId = model.OwnerId;
+
+        _dbContext.ownerCars.Update(result);
+        var res = await _dbContext.SaveChangesAsync();
+        return res;
+    }
+}
