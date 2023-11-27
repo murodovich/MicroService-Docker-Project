a2fd19c766151b76fd5113fc21cf2c5f818dac02
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:45:54 +0500

    dadas

--- Car-Api/Program.cs
+++ Car-Api/Program.cs
@@ -1,3 +1,10 @@
+using Car_Application.Data;
+using Car_Application.Repositories.CarRepositories;
+using Car_Application.Repositories.ManufacturerRepositories;
+using Car_Application.Repositories.OwnerCarRepositories;
+using Car_Application.Repositories.OwnerRepositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CarDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
+builder.Services.AddScoped<IOwnerCarRepository, OwnerCarRepository>();
+builder.Services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
 
 var app = builder.Build();
 
