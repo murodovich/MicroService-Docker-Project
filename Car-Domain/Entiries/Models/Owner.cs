160028105dbb1f2e7772e369a92d6fa1d2c6a222
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:47:28 +0500

    dadad

---
+++ Car-Domain/Entiries/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace Car_Domain.Entiries.Models;
+public class Owner
+{
+    public int OwnerId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public List<OwnerCar> OwnerCars { get; set; }
+}
