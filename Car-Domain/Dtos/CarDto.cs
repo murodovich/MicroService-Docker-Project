f13d8b45193a90df47d28ac5725dd2f709be67d4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:47:04 +0500

    dads

---
+++ Car-Domain/Dtos/CarDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Car_Domain.Dtos;
+public class CarDto
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+    public int ManufacturerId { get; set; }
+}
