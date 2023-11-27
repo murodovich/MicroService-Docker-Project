2e984f5226d4153b0f3463539d3a8c92a071a1fa
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:46:02 +0500

    adsad

---
+++ Car-Application/Data/CarDBContext.cs
@@ -0,0 +1,42 @@
+﻿using Car_Domain.Entiries.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Car_Application.Data;
+public class CarDBContext : DbContext
+{
+    public CarDBContext(DbContextOptions<CarDBContext> options) : base(options)
+    {
+
+    }
+    public DbSet<Car> cars { get; set; }
+    public DbSet<Owner> owner { get; set; }
+    public DbSet<Manufacturer> manufacturer { get; set; }
+    public DbSet<OwnerCar> ownerCars { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Car model uchun Manufacturer bo'yicha indeks qo'shish
+        modelBuilder.Entity<Car>()
+            .HasIndex(c => c.ManufacturerId)
+            .IsUnique(false);
+
+        // Owner va Car modellari uchun Many-to-Many bog'lanishni o'zgartirish
+        modelBuilder.Entity<OwnerCar>()
+            .HasKey(oc => new { oc.OwnerId, oc.CarId });
+
+        modelBuilder.Entity<OwnerCar>()
+            .HasOne(oc => oc.Owner)
+            .WithMany(o => o.OwnerCars)
+            .HasForeignKey(oc => oc.OwnerId);
+
+        modelBuilder.Entity<OwnerCar>()
+            .HasOne(oc => oc.Car)
+            .WithMany(c => c.OwnedBy)
+            .HasForeignKey(oc => oc.CarId);
+
+        // Boshqa konfiguratsiyalar...
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+}
