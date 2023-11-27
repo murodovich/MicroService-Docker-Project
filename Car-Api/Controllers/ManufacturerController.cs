6cb61ad084a0ae0f6379ea0e2b525df8859f7946
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:32:27 +0500

    sdf

---
+++ Car-Api/Controllers/ManufacturerController.cs
@@ -0,0 +1,49 @@
+﻿using Car_Application.Repositories.ManufacturerRepositories;
+using Car_Domain.Dtos;
+using Car_Domain.Entiries.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ManufacturerController : ControllerBase
+    {
+        private readonly IManufacturerRepository _manufacturerRepository;
+
+        public ManufacturerController(IManufacturerRepository manufacturerRepository)
+        {
+            _manufacturerRepository = manufacturerRepository;
+        }
+        [HttpGet]
+        public IActionResult ManufacturerGetAll()
+        {
+            var result = _manufacturerRepository.GetAllAsync();
+            return Ok(result.Result);
+        }
+        [HttpPost]
+        public IActionResult ManufacturerCreated(ManufacturerDto manufacturerDto)
+        {
+            var result = _manufacturerRepository.CreateAsync(manufacturerDto);
+            return Ok(result.Result);
+        }
+        [HttpGet]
+        public IActionResult ManufacturerGetById(int id)
+        {
+            var result = _manufacturerRepository.GetByIdAsync(id);
+            return Ok(result.Result);
+        }
+        [HttpPut]
+        public IActionResult ManufacturerUpdated(int id, ManufacturerDto manufacturerDto)
+        {
+            var result  = _manufacturerRepository.UpdateAsync(id, manufacturerDto);
+            return Ok(result.Result);
+        }
+        [HttpDelete]
+        public IActionResult ManufacturerDeleted(int id)
+        {
+            var result = _manufacturerRepository.DeleteAsync(id);
+            return Ok(result.Result);
+        }
+    }
+}
