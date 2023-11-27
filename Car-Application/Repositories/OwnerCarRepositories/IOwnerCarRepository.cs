c45b57ece91a1bd83365b4159d80764f368dadfb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:46:39 +0500

    dasd

---
+++ Car-Application/Repositories/OwnerCarRepositories/IOwnerCarRepository.cs
@@ -0,0 +1,7 @@
+﻿using Car_Domain.Dtos;
+using Car_Domain.Entiries.Models;
+
+namespace Car_Application.Repositories.OwnerCarRepositories;
+public interface IOwnerCarRepository : IBaseRepository<OwnerCar,OwnerCarDto>
+{
+}
