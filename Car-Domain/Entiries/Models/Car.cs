5f09d522bc30cd1c608377d17f1f4a891f56be38
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:47:21 +0500

    adsads

---
+++ Car-Domain/Entiries/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace Car_Domain.Entiries.Models;
+public class Car
+{
+    public int CarId { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+
+    public int ManufacturerId { get; set; }
+    public Manufacturer Manufacturer { get; set; }
+
+    public List<OwnerCar> OwnedBy { get; set; }
+}
