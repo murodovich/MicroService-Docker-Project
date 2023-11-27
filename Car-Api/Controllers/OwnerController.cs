5e5baa34f1f73e9e070d8efe82068af14a18c345
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:32:38 +0500

    sfdsf

---
+++ Car-Api/Controllers/OwnerController.cs
@@ -0,0 +1,44 @@
+﻿using Car_Application.Repositories.OwnerCarRepositories;
+using Car_Application.Repositories.OwnerRepositories;
+using Car_Domain.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private readonly IOwnerRepository _ownerRepo;
+
+        public OwnerController(IOwnerRepository ownerRepo)
+        {
+            _ownerRepo = ownerRepo;
+        }
+
+        [HttpGet]
+        public IActionResult OwnerGetAll()
+        {
+            var result = _ownerRepo.GetAllAsync();
+            return Ok(result.Result);
+        }
+        [HttpPost]
+        public IActionResult OwnerCreated(OwnerDto ownerDto)
+        {
+            var result = _ownerRepo.CreateAsync(ownerDto);
+            return Ok(result.Result);
+        }
+        [HttpGet]
+        public IActionResult OwnerGetById(int id)
+        {
+            var result = _ownerRepo.GetByIdAsync(id);
+            return Ok(result.Result);
+        }
+        [HttpPut]
+        public IActionResult OwnerUpdated(int id, OwnerDto ownerDto)
+        {
+            var result = _ownerRepo.UpdateAsync(id, ownerDto);
+            return Ok(result.Result);
+        }
+    }
+}
