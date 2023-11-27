8ef35ca0cf319e3e247d6fb6e994dfa339e0138b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-27 21:32:18 +0500

    dsfs

--- Appartment-Api/Program.cs
+++ Appartment-Api/Program.cs
@@ -1,4 +1,5 @@
 using Appartment_Application.Repositories.ApartmentRepositories;
+using Appartment_Application.Repositories.PaymentRepositories;
 using Appartment_Application.Repositories.UserRepositories;
 using Appartment_Domain.Data;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IApartmentRepository, ApartmentRepository>();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+
 
 
 builder.Services.AddDbContext<ApartmentDBContext>(options =>
