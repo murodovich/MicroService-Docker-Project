681b67b43616399ad7de272b4a8284d46290a635
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:55:49 +0500

    jhkadskj ad ka

---
+++ Tourism-Domain/Entities/Models/Destination.cs
@@ -0,0 +1,9 @@
+﻿namespace Tourism_Domain.Entities.Models;
+public class Destination
+{
+    public int DestinationId { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Description { get; set; }
+}
