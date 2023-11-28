a681f1f283dacdc085ce79f938bc764b2b71ad3f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:54:39 +0500

    jbka dskj

---
+++ Tourism-Application/Dtos/BookingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Tourism_Application.Dtos;
+public class BookingDto
+{
+    public int TourPackageId { get; set; }
+    public int HotelId { get; set; }
+    public int RequiredRoomCount { get; set; }
+    public decimal BookingPrice { get; set; }
+    public DateTime BookingDate { get; set; }
+}
