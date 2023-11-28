b8f62ea94fdaa7ed9ed80c940cd1298ae8d0a57d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:56:22 +0500

    hksa dk has

---
+++ Tourism-Infrastructure/Services/IBaseservice.cs
@@ -0,0 +1,10 @@
+﻿namespace Tourism_Infrastructure.Services;
+public interface IBaseservice<Tmodel,TmodelDto> where TmodelDto : class
+{
+    public ValueTask<bool> Create(TmodelDto dto);
+    public ValueTask<bool> Update(int id, TmodelDto dto);
+    public ValueTask<bool> Delete(int id);
+    public ValueTask<List<Tmodel>> GetAll();
+    public ValueTask<Tmodel> GetById(int id);
+
+}
