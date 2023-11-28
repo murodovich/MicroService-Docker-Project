9cb990933e84417c7bd9db18f78b7078dbfc82d6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:54:46 +0500

    ajkdbaj

---
+++ Tourism-Application/Dtos/DestinationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Tourism_Application.Dtos;
+public class DestinationDto
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string City { get; set; }
+    public string Description { get; set; }
+}
