ef465e158172f6fc33dc7749ed3a726926c3c3c0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:55:27 +0500

    ads kjad k kj da

---
+++ Tourism-Application/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Tourism_Application.Repositories
+{
+    public interface IBaseRepository<Tmodel,TView> where Tmodel : class
+    {
+        public ValueTask<bool> CreateAsync(Tmodel model);
+        public ValueTask<bool> UpdateAsync(int id,Tmodel model);
+        public ValueTask<bool> DeleteAsync(int id);
+        public ValueTask<Tmodel> GetByIdAsync(int id);
+        public ValueTask<List<Tmodel>> GetAllAsync();
+
+    }
+}
