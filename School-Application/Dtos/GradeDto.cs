c6342e1d80e05ee24b7b55dab06d5a4aac6ea52c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:48:03 +0500

    adsa

---
+++ School-Application/Dtos/GradeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace School_Application.Dtos;
+public class GradeDto
+{
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public DateTime GradeDate { get; set; }
+    public float Score { get; set; }
+}
