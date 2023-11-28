89e91190a85f2afffff30fe8a89c9fa8656b3a7f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:56:17 +0500

    khadkja jn

---
+++ Tourism-Infrastructure/Services/HotelServices/IHotelService.cs
@@ -0,0 +1,7 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Infrastructure.Services.HotelServices;
+public interface IHotelService : IBaseservice<Hotel,HotelDto>
+{
+}
