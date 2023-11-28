cc66d0365029cf07cf1e1ec9b182d734aa297a35
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:54:03 +0500

    jnlna

---
+++ Tourism-Api/Controllers/HotelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tourism_Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        
+        
+    }
+}
