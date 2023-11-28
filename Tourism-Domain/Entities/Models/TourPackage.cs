c0f18318c3cbe2be1733a672b6d96eda9199efbd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:56:00 +0500

    jbkjasd kj

---
+++ Tourism-Domain/Entities/Models/TourPackage.cs
@@ -0,0 +1,9 @@
+﻿namespace Tourism_Domain.Entities.Models;
+public class TourPackage
+{
+    public int TourPackageId { get; set; }
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Price { get; set; }
+}
