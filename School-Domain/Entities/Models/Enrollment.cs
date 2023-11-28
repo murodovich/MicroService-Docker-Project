10b49bdc2ae0b4d4d5eb64768fee867132021d89
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:49:21 +0500

    uighi

---
+++ School-Domain/Entities/Models/Enrollment.cs
@@ -0,0 +1,8 @@
+﻿namespace School_Domain.Entities.Models;
+public class Enrollment
+{
+    public int EnrollmentId { get; set; }
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+}
