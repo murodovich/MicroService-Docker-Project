2830ae60b181337e9dca75b9db3bc36a815719f0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:54:57 +0500

    djkabdkj k j  akdj

---
+++ Tourism-Application/Dtos/TourPackageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Tourism_Application.Dtos;
+public class TourPackageDto
+{
+    public string Name { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public decimal Price { get; set; }
+}
