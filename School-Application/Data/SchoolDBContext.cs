46ba9cb0db4a8b11d6b786076895123f0a216201
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:47:48 +0500

    imiom

---
+++ School-Application/Data/SchoolDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School_Domain.Entities.Models;
+
+namespace School_Application.Data;
+public class SchoolDBContext : DbContext
+{
+    public SchoolDBContext(DbContextOptions<SchoolDBContext> options) : base(options)
+    {
+        
+    }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<Grade> Grades { get; set; }
+
+}
