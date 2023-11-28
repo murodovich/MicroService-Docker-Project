aa48ff43a8069653ace77952ab1db9a4d69a51e5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:55:55 +0500

    jjbkdab

---
+++ Tourism-Domain/Entities/Models/Hotel.cs
@@ -0,0 +1,9 @@
+﻿namespace Tourism_Domain.Entities.Models;
+public class Hotel
+{
+    public int HotelId { get; set; }
+    public string Name { get; set; }
+    public int DestinationId { get; set; }
+    public int StarRating { get; set; }
+    public decimal PricePerNight { get; set; }
+}
