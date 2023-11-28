884ced4e5f2e16e06914e810c12b2561056c3771
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:55:44 +0500

    ankdbkjad

---
+++ Tourism-Domain/Entities/Models/Booking.cs
@@ -0,0 +1,10 @@
+﻿namespace Tourism_Domain.Entities.Models;
+public class Booking
+{
+    public int BookingId { get; set; }
+    public int TourPackageId { get; set; }
+    public int HotelId { get; set; }
+    public int RequiredRoomCount { get; set; }
+    public decimal BookingPrice { get; set; }
+    public DateTime BookingDate { get; set; }
+}
