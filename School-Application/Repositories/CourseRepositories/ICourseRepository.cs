84f8b64da5c1fa698c0951055f49785e23d5dd13
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:48:29 +0500

    dadsa

---
+++ School-Application/Repositories/CourseRepositories/ICourseRepository.cs
@@ -0,0 +1,7 @@
+﻿using School_Application.Dtos;
+using School_Domain.Entities.Models;
+
+namespace School_Application.Repositories.CourseRepositories;
+public interface ICourseRepository : IBaseRepository<Course,CourseDto>
+{
+}
