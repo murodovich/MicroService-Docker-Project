af1a9130fa4088028fb9e055cd0b8013acc1f4a7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:47:59 +0500

    dfz

---
+++ School-Application/Dtos/EnrollmentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace School_Application.Dtos;
+public class EnrollmentDto
+{
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+
+}
