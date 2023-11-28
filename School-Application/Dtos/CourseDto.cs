f1d717db076dcb4f5d9715f1ac4fc35270265697
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:47:55 +0500

    uni

---
+++ School-Application/Dtos/CourseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace School_Application.Dtos;
+public class CourseDto
+{
+    public string CourseName { get; set; }
+    public string Instructor { get; set; }
+    public int Credits { get; set; }
+}
