a0b466f995538047020d8be15b45eb34be666565
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:48:07 +0500

    dsad

---
+++ School-Application/Dtos/StudentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace School_Application.Dtos;
+public class StudentDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Group { get; set; }
+}
