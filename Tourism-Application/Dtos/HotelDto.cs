ba17f38d419e50658879ed159cf1129b10f29f72
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:54:52 +0500

    kjbajdskj  bm

---
+++ Tourism-Application/Dtos/HotelDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Tourism_Application.Dtos;
+public class HotelDto
+{
+    public string Name { get; set; }
+    public int DestinationId { get; set; }
+    public int StarRating { get; set; }
+    public decimal PricePerNight { get; set; }
+}
