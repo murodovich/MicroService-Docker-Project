71c5f566a197d934df4eac0d5f2f812f9ccf35a3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:53:49 +0500

    hb kjad

--- Schools-Api/Program.cs
+++ Schools-Api/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using School_Application.Data;
+using School_Application.Repositories.CourseRepositories;
+using School_Application.Repositories.EnrollmentRepositories;
+using School_Application.Repositories.GradeRepositories;
+using School_Application.Repositories.StudentRepositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+builder.Services.AddScoped<ICourseRepository, CourseRepository>();
+builder.Services.AddScoped<IGradeRepository, GradeRepository>();
+builder.Services.AddScoped<IEnrollmentRepository,EnrollmenRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<SchoolDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
