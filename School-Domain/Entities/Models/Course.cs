ee7680686b8f31836b465c3f83a194cedc1c20d8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:49:15 +0500

    dadad

---
+++ School-Domain/Entities/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿namespace School_Domain.Entities.Models;
+public class Course
+{
+    public int CourseId { get; set; }
+    public string CourseName { get; set; }
+    public string Instructor { get; set; }
+    public int Credits { get; set; }
+
+}
