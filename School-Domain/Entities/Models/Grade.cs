81baed8554deb831988df3ec73e17b54609f7c56
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:52:48 +0500

    jhvjhv

---
+++ School-Domain/Entities/Models/Grade.cs
@@ -0,0 +1,9 @@
+﻿namespace School_Domain.Entities.Models;
+public class Grade
+{
+    public int GradeId { get; set; }
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public DateTime GradeDate { get; set; }
+    public float Score { get; set; }
+}
