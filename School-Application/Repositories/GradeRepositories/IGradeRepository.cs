9f5e258beaf672e53dee5ae51ee484adc498e4fc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:48:49 +0500

    dada

---
+++ School-Application/Repositories/GradeRepositories/IGradeRepository.cs
@@ -0,0 +1,7 @@
+﻿using School_Application.Dtos;
+using School_Domain.Entities.Models;
+
+namespace School_Application.Repositories.GradeRepositories;
+public interface IGradeRepository : IBaseRepository<Grade,GradeDto>
+{
+}
