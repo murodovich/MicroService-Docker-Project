6715bd2aa7ea9b5b66a90231df16757cafc08503
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:54:34 +0500

    dkbakhd khbkba

---
+++ Tourism-Application/Data/TourismDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Application.Data;
+public class TourismDBContext : DbContext
+{
+    public TourismDBContext(DbContextOptions<TourismDBContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Hotel> hotels { get; set; }
+    public DbSet<Booking> bookings { get; set; }
+    public DbSet<TourPackage> package { get; set; }
+    public DbSet<Destination> destinations { get; set; }
+
+
+}
