f51372755eac41a508b7da240738d5833b27676d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:52:53 +0500

    yc

---
+++ School-Domain/Entities/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace School_Domain.Entities.Models;
+public class Student
+{
+    public int StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Group { get; set; }
+}
