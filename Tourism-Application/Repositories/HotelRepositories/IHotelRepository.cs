ba144fd028c0d270b9965171d3bfc47aab6dbc18
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:55:22 +0500

    khn ka dbh

---
+++ Tourism-Application/Repositories/HotelRepositories/IHotelRepository.cs
@@ -0,0 +1,7 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Application.Repositories.HotelRepositories;
+public interface IHotelRepository : IBaseRepository<Hotel,HotelDto>
+{
+}
