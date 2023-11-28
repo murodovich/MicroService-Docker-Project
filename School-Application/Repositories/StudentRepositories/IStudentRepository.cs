4be5f23980754515d02304abe7c21ec9dd30ad65
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:48:54 +0500

    dada

---
+++ School-Application/Repositories/StudentRepositories/IStudentRepository.cs
@@ -0,0 +1,7 @@
+﻿using School_Application.Dtos;
+using School_Domain.Entities.Models;
+
+namespace School_Application.Repositories.StudentRepositories;
+public interface IStudentRepository : IBaseRepository<Student,StudentDto>
+{
+}
