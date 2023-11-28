fc0bf9ba376dadd532ac75cc427d52f867cf8935
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-28 13:48:39 +0500

    adsada

---
+++ School-Application/Repositories/EnrollmentRepositories/IEnrollmentRepository.cs
@@ -0,0 +1,7 @@
+﻿using School_Application.Dtos;
+using School_Domain.Entities.Models;
+
+namespace School_Application.Repositories.EnrollmentRepositories;
+public interface IEnrollmentRepository :IBaseRepository<Enrollment,EnrollmentDto>
+{
+}
