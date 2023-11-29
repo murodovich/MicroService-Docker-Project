43f19f27015984ec9856ed1182dd2156d84f9b59
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-29 15:15:08 +0500

    Booking Repo

---
+++ Tourism-Infrastructure/Services/BookingServices/BookingService.cs
@@ -0,0 +1,58 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Application.Repositories.BookingRepositories;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Infrastructure.Services.BookingServices
+{
+    public class BookingService : IBookingService
+    {
+        private readonly IBookingRepository _repository;
+
+        public BookingService(IBookingRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ValueTask<bool> Create(BookingDto dto)
+        {
+            Booking booking = new Booking();
+            booking.BookingDate = dto.BookingDate;
+            booking.BookingPrice = dto.BookingPrice;
+            booking.TourPackageId = dto.TourPackageId;
+            booking.HotelId = dto.HotelId;
+
+            var result = _repository.CreateAsync(booking);
+            return result;
+        }
+
+        public ValueTask<bool> Delete(int id)
+        {
+            var result = _repository.DeleteAsync(id);
+            return result;
+        }
+
+        public ValueTask<List<Booking>> GetAll()
+        {
+            var result = _repository.GetAllAsync();
+            return result;
+        }
+
+        public ValueTask<Booking> GetById(int id)
+        {
+            var result = _repository.GetByIdAsync(id);
+            return result;
+        }
+
+        public ValueTask<bool> Update(int id, BookingDto dto)
+        {
+            Booking booking = new Booking();
+            booking.BookingDate = dto.BookingDate;
+            booking.BookingPrice = dto.BookingPrice;
+            booking.TourPackageId = dto.TourPackageId;
+            booking.HotelId = dto.HotelId;
+
+            var result = _repository.UpdateAsync(id, booking);
+            return result;
+        }
+    }
+}
---
+++ Tourism-Infrastructure/Services/BookingServices/IBookingService.cs
@@ -0,0 +1,9 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Infrastructure.Services.BookingServices
+{
+    public interface IBookingService : IBaseservice<Booking,BookingDto>
+    {
+    }
+}
