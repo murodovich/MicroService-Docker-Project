19e248bc96bbffeddacf530e2570c2e78fb452e4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-29 15:14:46 +0500

    Booking Repo

---
+++ Tourism-Application/Repositories/BookingRepositories/IBookingRepository.cs
@@ -0,0 +1,9 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Application.Repositories.BookingRepositories
+{
+    public interface IBookingRepository : IBaseRepository<Booking,BookingDto>
+    {
+    }
+}
