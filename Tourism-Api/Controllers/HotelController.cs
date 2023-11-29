da9c4612a81c97bdfef28cc7700ac86635b01d97
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-29 15:13:40 +0500

    Hotel Controller

--- Tourism-Api/Controllers/HotelController.cs
+++ Tourism-Api/Controllers/HotelController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Tourism_Application.Dtos;
+using Tourism_Infrastructure.Services.HotelServices;
 
 namespace Tourism_Api.Controllers
 {
@@ -6,7 +8,44 @@ namespace Tourism_Api.Controllers
     [ApiController]
     public class HotelController : ControllerBase
     {
-        
-        
+        private readonly IHotelService _hotelService;
+
+        public HotelController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpGet]
+        public IActionResult HotelGetAll()
+        {
+
+            var res = _hotelService.GetAll();
+            return Ok(res.Result);
+        }
+        [HttpPost]
+        public IActionResult HotelCreate(HotelDto hotelDto)
+        {
+            var result = _hotelService.Create(hotelDto);
+            return Ok(result.Result);
+        }
+        [HttpGet]
+        public IActionResult HotelGetById(int id)
+        {
+            var result = _hotelService.GetById(id);
+            return Ok(result.Result);
+        }
+        [HttpPut]
+        public IActionResult HotelUpdate(int id, HotelDto hotelDto)
+        {
+            var result = _hotelService.Update(id, hotelDto);
+            return Ok(result.Result);
+        }
+        [HttpDelete]
+        public IActionResult HotelDelete(int id)
+        {
+            var result = _hotelService.Delete(id);
+            return Ok(result.Result);
+        }
+
     }
 }
