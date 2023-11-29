93d4c0526de23cbf87ef84b4cf70df9129bab527
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-29 15:15:20 +0500

    Destination Service

---
+++ Tourism-Infrastructure/Services/DestinationServices/DestinationService.cs
@@ -0,0 +1,58 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Application.Repositories.DestinationRepositories;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Infrastructure.Services.DestinationServices
+{
+    public class DestinationService : IDestinationService
+    {
+        private readonly IDestinationRepository _destinationRepository;
+
+        public DestinationService(IDestinationRepository destinationRepository)
+        {
+            _destinationRepository = destinationRepository;
+        }
+
+        public ValueTask<bool> Create(DestinationDto dto)
+        {
+            Destination destination = new Destination();
+            destination.Name = dto.Name;
+            destination.Description = dto.Description;
+            destination.City = dto.City;
+            destination.Country = dto.Country;
+
+            var result = _destinationRepository.CreateAsync(destination);
+            return result;
+        }
+
+        public ValueTask<bool> Delete(int id)
+        {
+            var result = _destinationRepository.DeleteAsync(id);
+            return result;
+        }
+
+        public ValueTask<List<Destination>> GetAll()
+        {
+            var result = _destinationRepository.GetAllAsync();
+            return result;
+        }
+
+        public ValueTask<Destination> GetById(int id)
+        {
+            var result = _destinationRepository.GetByIdAsync(id);
+            return result;
+        }
+
+        public ValueTask<bool> Update(int id, DestinationDto dto)
+        {
+            Destination destination = new Destination();
+            destination.Name = dto.Name;
+            destination.Description = dto.Description;
+            destination.City = dto.City;
+            destination.Country = dto.Country;
+            
+            var result = _destinationRepository.UpdateAsync(id, destination);
+            return result;
+        }
+    }
+}
---
+++ Tourism-Infrastructure/Services/DestinationServices/IDestinationService.cs
@@ -0,0 +1,9 @@
+﻿using Tourism_Application.Dtos;
+using Tourism_Domain.Entities.Models;
+
+namespace Tourism_Infrastructure.Services.DestinationServices
+{
+    public interface IDestinationService : IBaseservice<Destination,DestinationDto>
+    {
+    }
+}
