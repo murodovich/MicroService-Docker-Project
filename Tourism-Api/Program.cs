7f73cb3ea159ee5c4b7b9c5a30708c0e823ccaaa
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-29 15:13:54 +0500

    P.sc

--- Tourism-Api/Program.cs
+++ Tourism-Api/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.EntityFrameworkCore;
 using Tourism_Application.Data;
+using Tourism_Application.Repositories.BookingRepositories;
+using Tourism_Application.Repositories.DestinationRepositories;
+using Tourism_Application.Repositories.HotelRepositories;
+using Tourism_Infrastructure.Services.BookingServices;
+using Tourism_Infrastructure.Services.DestinationServices;
+using Tourism_Infrastructure.Services.HotelServices;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +20,13 @@ builder.Services.AddDbContext<TourismDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IHotelRepository, HotelRepository>();
+builder.Services.AddScoped<IHotelService, HotelService>();
+builder.Services.AddScoped<IBookingService, BookingService>();
+builder.Services.AddScoped<IBookingRepository, BookingRepository>();    
+builder.Services.AddScoped<IDestinationRepository, DestinationRepository>();
+builder.Services.AddScoped<IDestinationService, DestinationService>();
 
 var app = builder.Build();
 
