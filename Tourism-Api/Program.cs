6fbeeb8d892181b07f437c319d3684ac6f9d7de3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 10:38:39 +0500

    Connection string Docker sql uchun

--- Tourism-Api/Program.cs
+++ Tourism-Api/Program.cs
@@ -10,10 +10,10 @@ using Tourism_Infrastructure.Services.HotelServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TourismDBContext>(options =>
@@ -30,12 +30,9 @@ builder.Services.AddScoped<IDestinationService, DestinationService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
